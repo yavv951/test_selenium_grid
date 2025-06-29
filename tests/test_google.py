597182d01ef91891dc2df7380d170ef0551e3571
yavv951 <vadimv95189@gmail.com>
2025-06-30 02:45:39 +0300

    fix compose files

--- tests/test_google.py
+++ tests/test_google.py
@@ -10,3 +10,13 @@ def test_google_title(driver):
 
     with allure.step("Проверяем заголовок"):
         assert "Google" in driver.title
+
+@allure.title("Проверка заголовка Not Google")
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.parametrize("driver", ["chrome", "firefox"], indirect=True)
+def test_google_title(driver):
+    with allure.step("Открываем Google"):
+        driver.get("https://www.google.com")
+
+    with allure.step("Проверяем заголовок"):
+        assert "Not Google" in driver.title
